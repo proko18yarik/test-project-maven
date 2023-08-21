fa5774ebad894d7ad601a6f145c9f4d826cf9972
Yaroslav Deren <derenyaroslav@gmail.com>
2023-08-21 03:33:04 +0300

    first commit

---
+++ core/src/main/java/org/example/ApacheFileUtilsExample.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+
+public class ApacheFileUtilsExample {
+
+    private static final String FILE_PATH = "C:\\Users\\Dell\\IdeaProjects\\mvn_test\\core\\src\\main\\java\\org\\example\\numb_song";
+    private static final String FILE_PATH_TO_CREATE = "C:\\Users\\Dell\\IdeaProjects\\mvn_test\\core\\src\\main\\java\\org\\example\\create_txt";
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(FILE_PATH);
+        List<String> lines = FileUtils.readLines(file);
+
+        lines.forEach(System.out::println);
+        File fileToCreate = new File(FILE_PATH_TO_CREATE);
+        FileUtils.touch(new File(FILE_PATH_TO_CREATE));
+    }
+}
---
+++ core/src/main/java/org/example/MSWorFileCreationExample.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+public class MSWorFileCreationExample {
+    private static final String FILE_PATH = "C:\\Users\\Dell\\IdeaProjects\\mvn_test\\core\\src\\main\\java\\org\\example\\create_txt";
+
+    public static void main(String[] args) {
+        XWPFDocument document = new XWPFDocument();
+        createTitle(document);
+        createSubTitle(document);
+
+        write(document);
+    }
+
+    private static void write(XWPFDocument document) {
+        try(FileOutputStream fos = new FileOutputStream(FILE_PATH)){
+            document.write(fos);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void createTitle(XWPFDocument document) {
+        XWPFParagraph title = document.createParagraph();
+        title.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFRun titleRun = title.createRun();
+        titleRun.setText("Creating Word Document");
+        titleRun.setColor("009933");
+        titleRun.setBold(true);
+        titleRun.setFontFamily("Courier");
+        titleRun.setFontSize(20);
+    }
+
+    private static void createSubTitle(XWPFDocument document) {
+        XWPFParagraph subTitle = document.createParagraph();
+        subTitle.setAlignment(ParagraphAlignment.CENTER);
+
+        XWPFRun subTitleRun = subTitle.createRun();
+        subTitleRun.setText("from HTTP fundamentals to API Mastery");
+        subTitleRun.setColor("00CC44");
+        subTitleRun.setFontFamily("Courier");
+        subTitleRun.setFontSize(16);
+        subTitleRun.setTextPosition(20);
+        subTitleRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
+    }
+}
+
